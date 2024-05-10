a4889432e10b0492f6e455a99bea38f9c6bcff6a
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-05-10 11:33:43 +0200

    Show recent events in chronological order

--- web/gissen/app/Http/Controllers/EventController.php
+++ web/gissen/app/Http/Controllers/EventController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use Carbon\Carbon;
 
 final class EventController extends Controller
 {
     public function index() {
-        $events = Event::all()->sortByDesc('start');
+        $yesterday = new Carbon('yesterday');
+        $events = Event::query()->where('start', '>=', $yesterday)->oldest('start')->get();
         return view('events', ['events' => $events]);
     }
 }
